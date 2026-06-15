100ba855f3049e7b5f45a9ceddf419ee9590839d
Angel Bulfone <mbulfone@gmail.com>
2026-06-14 22:22:58 -0700

    Build porymap

--- flake.nix
+++ flake.nix
@@ -23,10 +23,33 @@
           runHook postInstall
         '';
       };
+      porymap = with pkgs.qt6; pkgs.stdenv.mkDerivation {
+        pname = "porymap";
+        version = "6.3.1";
+        src = pkgs.fetchFromGitHub {
+          owner = "huderlem";
+          repo = "porymap";
+          tag = "6.3.1";
+          hash = "sha256-EG09aOgJrIe5X+e/SKcZn+mxkZ2N4mBmRxlEV3LYvgo=";
+        };
+        buildInputs = [ qtbase qtdeclarative ];
+        nativeBuildInputs = [ wrapQtAppsHook pkgs.installShellFiles qmake ];
+        installPhase = ''
+          runHook preInstall
+          installBin "porymap"
+          runHook postInstall
+        '';
+      };
     in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       name = "pokeemerald-expansion";
-      buildInputs = [ pkgs.libpng pkgs.pkg-config pkgs.pkgsCross.arm-embedded.stdenv.cc superfamiconv ];
+      buildInputs = [
+        pkgs.libpng
+        pkgs.pkg-config
+        pkgs.pkgsCross.arm-embedded.stdenv.cc
+        superfamiconv
+        porymap
+      ];
       ROMTEST = lib.getExe' rom-test "mgba-rom-test";
     };
   };
