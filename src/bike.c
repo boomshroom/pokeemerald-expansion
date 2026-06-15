16e1f8bafef8153141c1720eadf2cda3cd5ed7df
Angel Bulfone <mbulfone@gmail.com>
2026-06-14 22:28:03 -0700

    Dangerous running

--- src/bike.c
+++ src/bike.c
@@ -1216,9 +1216,8 @@ static bool8 IsRunningDisallowedByMetatile(u8 tile)
 {
     if (MetatileBehavior_IsRunningDisallowed(tile))
         return TRUE;
-    if (MetatileBehavior_IsFortreeBridge(tile) && (PlayerGetElevation() & 1) == 0)
-        return TRUE;
-    return FALSE;
+    else
+        return FALSE;
 }
 
 static void Bike_TryAdvanceCyclingRoadCollisions(void)
