38f39c57a6080815499bc49541076892d437a073
Angel Bulfone <mbulfone@gmail.com>
2026-06-17 15:09:32 -0700

    Smart Rayquaza

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1510,7 +1510,7 @@
 #define FLAG_RECEIVED_POKEDEX_FROM_BIRCH            (SYSTEM_FLAGS + 0x84)
 
 #define FLAG_CUSTOM_VICTORY_CATCH                   (SYSTEM_FLAGS + 0x85) // Unused Flag
-#define FLAG_UNUSED_0x8E6                           (SYSTEM_FLAGS + 0x86) // Unused Flag
+#define FLAG_CUSTOM_SMART_WILD                      (SYSTEM_FLAGS + 0x86) // Unused Flag
 #define FLAG_UNUSED_0x8E7                           (SYSTEM_FLAGS + 0x87) // Unused Flag
 #define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
 #define FLAG_UNUSED_0x8E9                           (SYSTEM_FLAGS + 0x89) // Unused Flag
